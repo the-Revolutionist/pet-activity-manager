8cedf4838353027d1b4c3f96520fbb47f28757a5
Vincnet J Lettau II <vjerome2@live.com>
2021-12-07 08:13:55 -0500

    adding activity is complete date output need to be reformatted

--- pet-activity-manager/src/App.js
+++ pet-activity-manager/src/App.js
@@ -2,28 +2,37 @@ import { Center, VStack } from '@chakra-ui/react';
 
 import { ActivityCard } from './ActivityCard';
 import { ActivityManager } from './ActivityManager';
+import { useState } from 'react';
 
-const activities = [
+const DUMMY_ACTIVITY = [
   {
     activity: 'Meal',
     note:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.' +
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-    date: Date(),
+    date: new Date(2021, 2, 28).getFullYear(),
   },
 ];
 
 function App() {
-  const newActivities = {};
-  const handleGetActivity = activity => {};
+  const [activities, setActivities] = useState(DUMMY_ACTIVITY);
+
+  const addActivityHandler = activity => {
+    setActivities(prevActivities => {
+      return [activity, ...prevActivities];
+    });
+
+    console.log(activities);
+  };
+
   return (
     <Center margin="20px">
       <VStack height="100%" width="500px">
         <ActivityManager
           activities={activities}
-          getActivity={handleGetActivity}
+          addActivity={addActivityHandler}
         />
-        {newActivities.map(activity => (
+        {activities.map(activity => (
           <ActivityCard
             activity={activity.activity}
             date={activity.date}
